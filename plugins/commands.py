a4c7aece7f2f452ee6bfee64f187f21e0c5329d9
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-07-23 06:57:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,11 +41,12 @@ async def start(client, message):
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
-    m=await message.reply_text(random.choice(SYD))
+    
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
+        m=await message.reply_text(random.choice(SYD))
         buttons = [[
                     InlineKeyboardButton('☒ Δᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ ☒', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
