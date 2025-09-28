6f08c6d5a9fcdff67b456ea4e504f885c22377cb
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-28 08:06:23 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2581,6 +2581,7 @@ async def auto_filter(client, msg, spoll=False):
             #search = search.replace("-", " ")
            # search = search.replace(":","")
             search = clean_text(search)
+            if search.strip() == "": return await sydm.delete()
             files, offset, total_results = await get_search_results(client, message.chat.id ,search, offset=0, filter=True)
             settings = await get_settings(message.chat.id)
             if not files:
