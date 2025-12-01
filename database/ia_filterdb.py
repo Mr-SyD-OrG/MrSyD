60438a9acf0abe002b0fc39bdf4795d06755714d
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-12-01 19:03:37 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -7,7 +7,7 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
+from info import DATABASE_URI, DATABASE_URI2, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
 from utils import get_settings, save_group_settings
 
 logger = logging.getLogger(__name__)
@@ -17,6 +17,10 @@ logger.setLevel(logging.INFO)
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
+
+clieent = AsyncIOMotorClient(DATABASE_URI2)
+bd = clieent[DATABASE_NAME]
+sydtance = sydtance.from_db(bd)
 
 @instance.register
 class Media(Document):
