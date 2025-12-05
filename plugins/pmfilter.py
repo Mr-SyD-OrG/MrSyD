a223bf28f2dbef2c44a2a2cfe346e52b68050424
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-12-05 07:44:51 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1142,7 +1142,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start=file_{file_id}")
     
     elif query.data.startswith("checksub"):
-        if not await is_req_subscribed(client, query):
+        if not await is_subscribed(client, query):
             await query.answer("Jᴏɪɴ ᴏᴜʀ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ ᴍᴀʜɴ! ᴩʟᴇᴀꜱᴇ... 🥺", show_alert=True)
             return
         ident, kk, file_id = query.data.split("#")
